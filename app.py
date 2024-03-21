66716ee36288e42e8c74d4140cdf9572f87473d7
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-03-21 15:00:56 +0000

    craicao da` pasta templates(html) e static (css + imagens)`

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Ola")
 
 @app.route("/")
-
+@app.route("/teste")
 def ola():
     return "Ola mundo,Bom dia"
+
+@app.route("/alunos")
+def alunos():
+    return render_template("hello.html")
