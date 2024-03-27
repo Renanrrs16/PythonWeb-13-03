ee6953c053bfbd6edebfc7e848a85e70083073bc
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-03-27 15:03:45 +0000

    Atualizacao do curso

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
 app = Flask("Ola")
 
-@app.route("/")
-@app.route("/teste")
-def ola():
-    return "Ola mundo,Bom dia"
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
 
-@app.route("/alunos")
-def alunos():
-    return render_template("hello.html")
+app.config.from_object(__name__)
+
+
+
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+def before_request():
+    g.bd = conectar()
+
+def teardown_request(f):
+    g.bd.close()
+
+@app.route("/")
+def Ola():
+    nomeUsuario = "Renan SOuza"
+    listaUsuario = ["Renan", "Oseas", "Renata", "Rosi"]
+    post = {"titulo": "Meu Titulo", "texto": "meu Texto", "data_criacao": "27/03/2024"}
+    return render_template("hello.html", post = post)
